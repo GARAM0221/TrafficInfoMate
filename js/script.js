93dc24e0ec5c63bee3b47a37aed0e2d433845fcf
GARAM0221 <161106051+GARAM0221@users.noreply.github.com>
2024-03-09 08:04:13 +0900

    index change

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+document.getElementById('routeForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // 폼 기본 제출 동작을 방지합니다.
+
+    var startPoint = document.getElementById('startPoint').value;
+    var endPoint = document.getElementById('endPoint').value;
+
+    // 여기에 출발지와 목적지를 사용하여 경로를 조회하는 코드를 추가합니다.
+    // 예: searchRoute(startPoint, endPoint);
+});
+
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
         center: new kakao.maps.LatLng(37.566535, 126.9779692), // 지도의 중심좌표
